c280722ef9f9f2b3b58ea2bd5e6df011b7686c84
Jeffery Nielsen <mesh_f@hotmail.com>
2016-05-25 08:20:53 +1000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,11 @@
-<h1>Rawr!</h1>
+<!doctype html>
+<html>
+<head>
+    <title>Mr.Jeffery Nielsen</title>
+</head>
+<body>
+    <h1>Rawr!</h1>
+    <p>I'm Jeffery Nielsen</p>
+</body>
+</html>
       
