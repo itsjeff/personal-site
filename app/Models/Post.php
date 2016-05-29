63f30a97c37cb545809a6b3f683e881900ba56a3
Jeff Nielsen <Jeff Nielsen>
2016-05-29 16:50:22 +1000

    Included soft delete in Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+	use SoftDeletes;
+	
 	protected $table = 'posts';	
+
+	protected $dates = ['deleted_at'];
 }
