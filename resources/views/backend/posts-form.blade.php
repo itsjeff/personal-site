64f997167d6f5a8a06f5d87e8a1e0654a4ec903b
Jeff Nielsen <mesh_f@hotmail.com>
2016-06-01 20:09:02 +1000

    detect post object

--- resources/views/backend/posts-form.blade.php
+++ resources/views/backend/posts-form.blade.php
@@ -8,11 +8,15 @@
 		        {!!csrf_field()!!}
 		        <div class="form-group">
 		        	<label class="form-label">Title</label>
-		        	<input class="form-control" type="text" name="title">
+		        	<input class="form-control" type="text" name="title" value="{{@if(isset($post)) $post->title @endif}}">
+		        </div>
+		        <div class="form-group">
+		        	<label class="form-label">Cover Image</label>
+		        	<input class="form-control" type="file" name="coverimage">
 		        </div>
 		        <div class="form-group">
 		        	<label class="form-label">Content</label>
-		        	<textarea class="form-control" rows="7" name="content"></textarea>
+		        	<textarea class="form-control" rows="7" name="content">{{@if(isset($post)) $post->content @endif}}</textarea>
 		        </div>
 		        <button class="btn btn-primary" type="submit" name="save">Save</button>
 		    </form>
