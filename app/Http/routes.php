5210740573cb5042a043c4fcf51aa2a848dd92ba
Jeff Nielsen <mesh_f@hotmail.com>
2016-07-01 20:56:36 +1000

    Added routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,12 @@
 */
 
 Route::get('/', 'Frontend\HomeController@index');
+Route::get('/categories/{slug?}', 'Frontend\CategoriesController@show');
+Route::get('/{slug}', 'Frontend\PostController@show');
 
+/**
+ * Auth
+ */
 Route::group(['namespace' => 'Auth'], function() {
 	Route::get('login', 'AuthController@getLogin');
 	Route::post('login', 'AuthController@postLogin');
@@ -21,10 +26,14 @@ Route::group(['namespace' => 'Auth'], function() {
 	Route::post('register', 'AuthController@postRegister');
 });
 
+/**
+ * Backend
+ */
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-	Route::get('/', function() {
-		echo 'asfas';
-	});
-
+	Route::get('/', 'Backend\DashboardController@index');
 	Route::resource('posts', 'Backend\PostsController');
+	Route::resource('media', 'Backend\MediaController');
+	Route::resource('categories', 'Backend\CategoriesController');
+	Route::resource('users', 'Backend\UsersController');
+	Route::resource('user-groups', 'Backend\UserGroupsController');
 });
