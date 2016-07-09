885c12e663ca8135292c9a19dbd9d3cf2eefe622
Jeff Nielsen <mesh_f@hotmail.com>
2016-07-09 14:57:15 +1000

    Auth has own master layout

---
+++ resources/views/auth/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Welcome Guest</title>
+    <link rel="stylesheet" href="/assets/icons/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/backend/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/backend/css/main.css">
+</head>
+<body id="app-layout">
+    <div class="content">
+        @yield('content')
+    </div>
+
+    <!-- JavaScripts -->
+    <script src="/assets/scripts/jquery-2.2.4.min.js"></script>
+    <script src="/assets/backend/js/bootstrap.min.js"></script>
+</body>
+</html>
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('auth.layouts.app')
 
 <!-- Main Content -->
 @section('content')
