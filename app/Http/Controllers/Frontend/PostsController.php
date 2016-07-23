f7a15ab6cde6908d8d0bbe5c6e89f237440843e9
Jeff Nielsen <mesh_f@hotmail.com>
2016-07-23 16:10:12 +1000

    Display post based on slug

--- app/Http/Controllers/Frontend/PostsController.php
+++ app/Http/Controllers/Frontend/PostsController.php
@@ -2,20 +2,41 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller as Controller;
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
+use App\Http\Controllers\Controller as Controller;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
+    /**
+     * Post model
+     */
+    protected $post;
+
+    /**
+     * Instantiate.
+     *
+     * @return void
+     */
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+	}
+
 	/**
-	 * Display single post
-	 * @param  [type] $slug [description]
-	 * @return [type]       [description]
+	 * Display single post.
+	 * 
+	 * @param  string  $slug  Display post based on slug.
+	 * @return void
 	 */
     public function show($slug)
     {
-    	echo $slug;
+    	$post = $this->post
+    		->where('slug', $slug)
+    		->first();
+
+        $this->setData('post', $post);
+
+        return view('frontend.post')->with($this->data);
     }
 }
